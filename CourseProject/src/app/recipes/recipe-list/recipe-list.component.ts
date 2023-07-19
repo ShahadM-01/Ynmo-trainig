c35cda38183fe2696fdd8d5e7946d4f4c3ab680f
ShahadM-01 <139268398+ShahadM-01@users.noreply.github.com>
2023-07-19 13:10:50 +0300

    Upload

--- CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A test', 'This is simply a test', 'https://i8b2m3d9.stackpathcdn.com/wp-content/uploads/2023/05/Soy_Milk_Hot_Pot_with_chicken_9128.jpg' )
+  ];
 
   ngOnInit() { }
 }
