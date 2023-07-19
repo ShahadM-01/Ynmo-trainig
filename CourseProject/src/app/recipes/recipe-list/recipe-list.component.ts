b65caa1e58838baa02b21056c6a9627480aaffd9
ShahadM-01 <139268398+ShahadM-01@users.noreply.github.com>
2023-07-19 13:26:54 +0300

    Upload

--- CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test', 'This is simply a test', 'https://i8b2m3d9.stackpathcdn.com/wp-content/uploads/2023/05/Soy_Milk_Hot_Pot_with_chicken_9128.jpg' )
+    new Recipe('A test', 'This is simply a test', 'https://i8b2m3d9.stackpathcdn.com/wp-content/uploads/2023/05/Soy_Milk_Hot_Pot_with_chicken_9128.jpg'),
+    new Recipe('A test', 'This is simply a test', 'https://i8b2m3d9.stackpathcdn.com/wp-content/uploads/2023/05/Soy_Milk_Hot_Pot_with_chicken_9128.jpg')
   ];
 
   ngOnInit() { }
