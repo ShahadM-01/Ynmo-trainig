3b56453cb92c4a81e239eaf2e329c31bda8b06e7
ShahadM-01 <139268398+ShahadM-01@users.noreply.github.com>
2023-07-24 22:55:49 +0300

    Upload

--- CourseProject/src/app/shared/dropdown.directive.ts
+++ CourseProject/src/app/shared/dropdown.directive.ts
@@ -10,3 +10,8 @@ export class DropdownDirectiv{
 }
 constructor(private elRef: ElementRef){}
 }
+
+// if you click on a component and you want to check if the click occurred within the component
+//  or outside of it, use `this.elRef.nativeElement.contains(event.target)` to determine this. 
+//  If the method returns `true`, then the click occurred within the component, and if it returns `false`, 
+//  then the click occurred outside of the component.
