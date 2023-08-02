002eafc8f9a4b55a3bbdfe7370e94116c489a7b6
ShahadM-01 <139268398+ShahadM-01@users.noreply.github.com>
2023-08-02 09:52:50 +0300

    programmatic Navigation to the edit page

--- CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit,  } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipes.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -11,11 +12,14 @@ import { RecipeService } from '../recipes.service';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
 
-  constructor(private resipSirvece: RecipeService) { }
+  constructor(private resipSirvece: RecipeService, private router:Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.recipes = this.resipSirvece.getRecipes();
   }
 
-
+  onNewRecipe(){
+    this.router.navigate(['new'], {relativeTo: this.route})
+  }
 }
