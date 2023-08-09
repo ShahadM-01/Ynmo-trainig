f3dbcb3b97ff7eb9d5a1cf9daa4166c5f90bc886
ShahadM-01 <139268398+ShahadM-01@users.noreply.github.com>
2023-08-10 00:02:38 +0300

    submissions

--- CourseProject/src/app/recipes/recipes.service.ts
+++ CourseProject/src/app/recipes/recipes.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
@@ -7,8 +7,7 @@ import { Subject } from 'rxjs';
 
 @Injectable()
 export class RecipeService {
-  // recipeSelected = new EventEmitter<Recipe>();
-
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe(
@@ -36,10 +35,24 @@ export class RecipeService {
 
   getRecipe(index: number) {
     return this.recipes[index];
-    
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
